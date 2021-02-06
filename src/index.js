f09f567d05e8a65fecc9a7b460a7ce755269a797
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-06 20:17:30 +0300

    feat: returns true and false correctly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var isNormal = true;
+  var newstr = str;
+  newstr = newstr.split("");
+  console.log(newstr);
+  for (var i = 0; i < newstr.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (newstr[i] == bracketsConfig[j][1] && newstr[i-1] == bracketsConfig[j][0]){
+        var removed = newstr.splice(i-1, 2);
+        i -= 2;
+      }
+    }
+  }
+  console.log(newstr);
+  if (newstr.length == 0) {
+    return isNormal;
+  } else {
+    return !isNormal;
+  }
 }
